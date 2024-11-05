5a83e18e6df6fc79f2ea52eae18ce5bf91280d32
David Hall <david.ryan.hall@gmail.com>
2024-11-04 18:11:37 -0800

    Security chain update to allow static assets through

--- src/main/java/com/radtimes/rad_times_server/config/SecurityConfig.java
+++ src/main/java/com/radtimes/rad_times_server/config/SecurityConfig.java
@@ -7,11 +7,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -19,24 +14,13 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .cors(Customizer.withDefaults())
+                .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                    .requestMatchers("/", "/index.html", "/static/rad-times-index.png", "/actuator/**").permitAll()
+                    .requestMatchers("/", "/index.html", "/static/**", "/actuator/**").permitAll()
                     .anyRequest().authenticated()
                 )
                 .oauth2Login(Customizer.withDefaults())
                 .build();
-    }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:8081"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 }
